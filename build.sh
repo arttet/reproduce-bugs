26b97fcc17e35a9e5df9af07bdbdf15a50c17b7d
Artyom Tetyukhin <51746822+arttet@users.noreply.github.com>
2021-09-06 13:01:56 +0300

    Reproduce the bug related to brew

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-conan install . --install-folder build --build=outdated -s arch_target=x86_64 -s build_type=Debug -s compiler.cppstd=17 -s compiler.version=12 -s compiler=clang -s compiler.libcxx=libstdc++11 -e CC=clang-12 -e CXX=clang++-12 
-conan build . --build-folder build
