f5315e2a5d86926e5622a6213660c212c8c8d207
Artyom Tetyukhin <51746822+arttet@users.noreply.github.com>
2021-09-22 22:01:17 +0300

    Add output about a branch of GIT

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
-conan install . --install-folder build --build=outdated -s arch_target=x86_64 -s build_type=Debug -s compiler.cppstd=17 -s compiler.version=12 -s compiler=clang -s compiler.libcxx=libstdc++11 -e CC=clang-12 -e CXX=clang++-12 
+# Clang 12.0.0
+conan install . -g cmake -if build -b outdated \
+    -s compiler.version=12 -s compiler=clang \
+    -s compiler.libcxx=libstdc++11 -s compiler.runtime=MDd \
+    -e CC=clang -e CXX=clang++ \
+    -e CONAN_CMAKE_GENERATOR=Ninja
+
 conan build . --build-folder build
