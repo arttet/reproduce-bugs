07ee2a7971c32b79c1e5a00ec534570cc3028bfa
Artyom Tetyukhin <51746822+arttet@users.noreply.github.com>
2021-10-28 00:36:25 +0300

    Solve this topic

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 # Visual Studio
-conan install . -g cmake -if build -b outdated \
+conan install . -if build \
     -s build_type=Debug -s arch_target=x86_64 -s compiler.cppstd=17 \
     -s compiler.version=19.29 -s compiler=msvc \
     -s compiler.runtime=dynamic -s compiler.runtime_type=Debug \
-    -e CONAN_CMAKE_GENERATOR=Ninja
-conan build . --build-folder build
+    -c tools.cmake.cmaketoolchain:generator=Ninja
+
+conan build . -bf build
--- conanfile.py
+++ conanfile.py
@@ -1,69 +1,30 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-from conans import ConanFile, CMake
+from conan.tools.cmake import CMake
+from conans import ConanFile
 
 
-class UtilsConan(ConanFile):
+class ReproduceBugsConan(ConanFile):
     name = "reproduce-bugs"
     version = "0.0.0"
-    description = "If you can reproduce a bug, you can fix It"
-    homepage = "https://github.com/arttet/reproduce-bugs"
+
+    # Optional metadata
     license = "MIT"
     author = "Artyom Tetyukhin"
-    settings = "os", "arch", "compiler", "build_type"
+    url = "https://github.com/arttet/reproduce-bugs"
+    description = "If you can reproduce a bug, you can fix It"
+    topics = ("cpp", "bugs")
 
-    options = {
-        "shared": [True, False],
-        "fPIC": [True, False],
-    }
-
-    default_options = {
-        "shared": False,
-        "fPIC": True,
-    }
-
-    generators = "cmake", "cmake_find_package"
+    # Binary configuration
+    generators = "CMakeDeps", "CMakeToolchain"
     build_policy = "missing"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
 
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
 
-    def configure(self):
-        pass
-
-    def requirements(self):
-        pass
-
-    def package_id(self):
-        pass
-
-    def build_requirements(self):
-        pass
-
-    def build_id(self):
-        pass
-
-    def system_requirements(self):
-        pass
-
-    def source(self):
-        pass
-
-    def imports(self):
-        pass
-
     def build(self):
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
-
-    def package(self):
-        pass
-
-    def package_info(self):
-        pass
-
-    def deploy(self):
-        pass
